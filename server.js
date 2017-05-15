5eaa8c4d64006a5db6176b2493cc2c5b4c9cb328
Marek294 <Markra5835@gmail.com>
2017-05-15 18:05:21 +0200

    change server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.set('views', './build')
 app.use(bodyParser.json());
 // Initialize routes
 app.get('/', function(req, res) {
-    res.sendFile(`/app/build/index.html`);
+    res.sendFile(path.join(__dirname,'build','index.html'));
 })
 app.use('/api', api);
 app.use('/panel', admin);
